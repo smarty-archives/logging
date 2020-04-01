40517eb92999cf74e1a1b73d24993d8f0957461c
Michael Whatcott <mdwhatcott@gmail.com>
2020-04-01 17:07:27 -0600

    Extracted helper functions.

--- logging_test.go
+++ logging_test.go
@@ -11,11 +11,8 @@ import (
 )
 
 func TestLoggingWithNilReferenceProducesTraditionalBehavior(t *testing.T) {
-	out := new(bytes.Buffer)
-	log.SetOutput(out)
-	log.SetFlags(0)
-	defer log.SetOutput(os.Stdout)
-	defer log.SetFlags(log.LstdFlags)
+	defer restore()
+	out := prepare()
 
 	thing := new(ThingUnderTest)
 	thing.Action()
@@ -24,28 +21,23 @@ func TestLoggingWithNilReferenceProducesTraditionalBehavior(t *testing.T) {
 }
 
 func TestLoggingWithLoggerCapturesOutput(t *testing.T) {
-	out := new(bytes.Buffer)
-	log.SetOutput(out)
-	log.SetFlags(0)
-	defer log.SetOutput(os.Stdout)
-	defer log.SetFlags(log.LstdFlags)
+	defer restore()
+	out := prepare()
 
 	thing := new(ThingUnderTest)
 	thing.log = Capture()
 	thing.Action()
 
 	assertions.New(t).So(thing.log.Log.String(), should.Equal, "Hello, World!\n")
-	assertions.New(t).So(out.Len(), should.Equal, 0)
+	assertNothingLoggedToStandardLogOutput(t, out)
 }
 
 func TestLoggingWithLoggerCapturesOutputInAllProvidedWriters(t *testing.T) {
-	out0 := new(bytes.Buffer)
+	defer restore()
+
+	out0 := prepare()
 	out1 := new(bytes.Buffer)
 	out2 := new(bytes.Buffer)
-	log.SetOutput(out0)
-	log.SetFlags(0)
-	defer log.SetOutput(os.Stdout)
-	defer log.SetFlags(log.LstdFlags)
 
 	thing := new(ThingUnderTest)
 	thing.log = Capture(out1, out2)
@@ -58,10 +50,12 @@ func TestLoggingWithLoggerCapturesOutputInAllProvidedWriters(t *testing.T) {
 	assertions.New(t).So(log0, should.Equal, "Hello, World!\n")
 	assertions.New(t).So(log1, should.Equal, "Hello, World!\n")
 	assertions.New(t).So(log2, should.Equal, "Hello, World!\n")
-	assertions.New(t).So(out0.Len(), should.Equal, 0)
+	assertNothingLoggedToStandardLogOutput(t, out0)
 }
 
 func TestLogCallsAreCounted(t *testing.T) {
+	defer restore()
+
 	thing := new(ThingUnderTest)
 	thing.log = Capture()
 	for x := 0; x < 10; x++ {
@@ -71,18 +65,33 @@ func TestLogCallsAreCounted(t *testing.T) {
 }
 
 func TestLoggingWithDiscard(t *testing.T) {
-	out := new(bytes.Buffer)
-	log.SetOutput(out)
-	log.SetFlags(0)
-	defer log.SetOutput(os.Stdout)
-	defer log.SetFlags(log.LstdFlags)
+	defer restore()
+	out := prepare()
 
 	thing := new(ThingUnderTest)
 	thing.log = Discard()
 	thing.Action()
 
 	assertions.New(t).So(thing.log.Log.Len(), should.Equal, 0)
-	assertions.New(t).So(out.Len(), should.Equal, 0)
+	assertNothingLoggedToStandardLogOutput(t, out)
+}
+
+func assertNothingLoggedToStandardLogOutput(t *testing.T, out *bytes.Buffer) bool {
+	return assertions.New(t).So(out.Len(), should.Equal, 0)
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+func restore() {
+	log.SetOutput(os.Stderr)
+	log.SetFlags(log.LstdFlags)
+}
+
+func prepare() *bytes.Buffer {
+	out := new(bytes.Buffer)
+	log.SetOutput(out)
+	log.SetFlags(0)
+	return out
 }
 
 /////////////////////////////////////////////////////////////////////////////////
